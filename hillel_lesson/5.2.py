6eaf2f9825f67eece8fdff5b1ace6ab4c7d7eb49
Maria_Zlobina <zlobinamaria@gmail.com>
2026-03-15 22:47:15 +0200

    Initial commit

---
+++ hillel_lesson/5.2.py
@@ -0,0 +1,28 @@
+while True:
+    number_1=int(input("Enter first number:"))
+    operator=input("Enter an operator:")
+    number_2=int(input("Enter second number:"))
+    for variable in operator:
+        match operator:
+            case "+":
+                number_1+number_2
+                print("Result=",number_1+number_2)
+            case "-":
+                number_1-number_2
+                print("Result=",number_1-number_2)
+            case "*":
+                number_1*number_2
+                print("Result=",number_1*number_2)
+            case "/":
+                match number_2:
+                    case 0:
+                        print("Result= error")
+                    case _:
+                        number_1/number_2
+                        print("Result=", number_1/number_2)
+    end=input("Press yes or y if you want to continue:")
+    if end != "yes" and end != "y":
+        break
+
+
+
